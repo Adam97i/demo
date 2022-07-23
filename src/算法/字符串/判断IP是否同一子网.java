5e07bdc4551bce9c335ac5db32de9d4bed97d1f9
Adam97i <1229427524@qq.com>
2022-07-23 18:50:47 +0800

    子网问题练习-优化代码结构

--- src/算法/字符串/判断IP是否同一子网.java
+++ src/算法/字符串/判断IP是否同一子网.java
@@ -15,11 +15,11 @@ public class 判断IP是否同一子网 {
 
         String mask = "255.0.0.0";
         String ip1 ="193.194.202.15";
-        String ip2 = "232.43.7.59";
+        String ip2 = "193.43.7.59";
 
-        boolean maskCheck = checkMask(mask);
-        boolean ip1Check = checkIp(ip1);
-        boolean ip2Check = checkIp(ip2);
+        boolean maskCheck = check(mask,true);
+        boolean ip1Check = check(ip1,false);
+        boolean ip2Check = check(ip2,false);
         if (!maskCheck || !ip1Check || !ip2Check) {
             System.out.println(1);
             return;
@@ -33,14 +33,13 @@ public class 判断IP是否同一子网 {
         } else {
             System.out.println(2);
         }
-
-
     }
 
     private static long ipToNum(String ip) {
         long res=0;
         long base=1;
-        String[] s = ip.split(".");
+        // 注意转义字符
+        String[] s = ip.split("\\.");
         int[] m = new int[s.length];
         for (int i =  s.length-1; i >=0; i--) {
             m[i] = Integer.parseInt(s[i]);
@@ -50,22 +49,7 @@ public class 判断IP是否同一子网 {
         return res;
     }
 
-    private static boolean checkMask(String mask) {
-        String[] s = mask.split("\\.");
-        int[] m = new int[s.length];
-        for (int i = 0; i < s.length; i++) {
-            m[i] = Integer.parseInt(s[i]);
-        }
-        for (int i : m) {
-            if (i < 0 || i > 255) {
-                return false;
-            }
-        }
-        // 掩码需要左边都是1 即每一段值都比后一段大
-        return m[0] >= m[1] && m[1] >= m[2] && m[2] >= m[3];
-    }
-
-    private static boolean checkIp(String ip) {
+    private static boolean check(String ip,boolean isMask) {
         String[] s = ip.split("\\.");
         int[] m = new int[s.length];
         for (int i = 0; i < s.length; i++) {
@@ -76,7 +60,10 @@ public class 判断IP是否同一子网 {
                 return false;
             }
         }
+        // 掩码需要左边都是1 即每一段值都比后一段大
+        if(isMask){
+            return m[0] >= m[1] && m[1] >= m[2] && m[2] >= m[3];
+        }
         return true;
     }
-
 }
