1e2eaf6f8bb86afff5a37f2ac4391039e8ce19e9
pengxinquan <pengxinquan@dqalpha.org>
2022-07-27 17:45:18 +0800

    集合遍历测试

--- src/collection/array.java
+++ src/collection/array.java
@@ -2,16 +2,27 @@ package collection;
 
 import java.util.ArrayList;
 
-/**
- * arraylist扩容测试
- */
 public class array {
     public static void main(String[] args) {
+        // arraylist扩容测试
         ArrayList<Integer> list = new ArrayList<>(20);
         for (int i = 0; i < 20; i++) {
             System.out.println(i);
             list.add(i);
         }
 
+        // forEach
+        ArrayList<String> list2 = new ArrayList<>();
+        list2.add("111");
+        list2.add("222");
+        list2.add("333");
+        // 普通for可以修改
+        for (int i = 0; i <list2.size(); i++) {
+            list2.remove("222");
+        }
+        // 增强for 锁定集合，不能修改删除元素，抛出CME
+        for(String i: list2){
+            list2.remove(i);
+        }
     }
 }
--- src/setPrint.java
+++ src/setPrint.java
@@ -1,18 +1,12 @@
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
 
 public class setPrint {
     public static void main(String[] args) {
-        Set<String> s=new HashSet<>();
-        s.add("张三");
-        s.add("张三");
-        s.add("张三");
-        s.add("张1");
-        s.add("张2");
-        s.add("张3");
-        String nameList = s.toString();
-        nameList = nameList.substring(1, nameList.length() - 2);
-        System.out.println(nameList);
+
+
+
 
         Integer a =Integer.valueOf(100);
         Integer b =Integer.valueOf(100);
